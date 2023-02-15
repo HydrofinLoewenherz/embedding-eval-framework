17324d418540014bfcfbef08c1ddb1bd6a55a4f5
Hydrofin <hydrofin@gmail.com>
2023-02-15 14:43:46 +0100

    remove old code and add graph for dist to pred

--- src/polar.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def z2polar(z: (float, float)) -> [float, float]:
-    rho = np.sqrt(z[0]**2 + z[1]**2)
-    phi = np.arctan2(z[1], z[0])
-    return [rho, phi]
-
-def zs2polars(zs: [(float, float)]) -> [float, float]:
-    return np.array([ z2polar(z) for z in zs ]).flatten()
