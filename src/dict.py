9f9550c491d291b671dbb84533834aa9007d572a
Hydrofin <hydrofin@gmail.com>
2023-02-23 14:37:04 +0100

    use pytorch

--- src/dict.py
+++
@@ -1,3 +0,0 @@
-# https://stackoverflow.com/a/36460020/10619052
-def list_to_dict(items: list) -> dict:
-    return {v: k for v, k in enumerate(items)}
--- src/square.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-import random
-
-def gen_square(amount: int) -> list[(float, float)]:
-    return [(random.uniform(0, 1), random.uniform(0, 1)) for _ in range(amount)]
